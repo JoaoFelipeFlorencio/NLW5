a770c5a475602651255332f682fa46900e7db7ac
Joao <florenciojoaofelipe@gmail.com>
2021-04-22 23:51:26 -0300

    Funcionalidades do player informacoes do podcast e tocando audio

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,16 +2,36 @@ import '../styles/global.scss';
 import styles from '../styles/app.module.scss';
 import { Header } from '../components/Header';
 import { Player } from '../components/Player';
+import { PlayerContext } from '../contexts/PlayerContext';
+import { useState } from 'react';
 
 function MyApp({ Component, pageProps }) {
+
+  const [episodeList, setEpisodeList] = useState([]);
+  const [currentEpisodeIndex, setCurrentEpisodeIndex] = useState(0);
+  const [isPlaying, setIsPlaying] = useState(false);
+
+  function play (episode){
+    setEpisodeList([episode]);
+    setCurrentEpisodeIndex(0);
+    setIsPlaying(true);
+  }
+
+  function togglePlay(){
+    setIsPlaying(!isPlaying);
+  }
+
   return (
-    <div className={styles.wrapper}>
-      <main>
-        <Header/>
-        <Component {...pageProps} />
-      </main>
-      <Player/>
-    </div>
+
+    <PlayerContext.Provider value ={ {episodeList, currentEpisodeIndex, isPlaying, togglePlay, play}}>
+      <div className={styles.wrapper}>
+        <main>
+          <Header/>
+          <Component {...pageProps} />
+        </main>
+        <Player/>
+      </div>
+    </PlayerContext.Provider>
   );
 }
 
